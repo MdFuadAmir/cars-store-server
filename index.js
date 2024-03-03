57333843b71faecd122a2dc3cc4973a436bc5d95
Md Fuad Amir <mdfuadamir@gmail.com>
2024-03-03 14:36:47 +0600

    added create and read oparation

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// ~~~~~~~~~~~~~~~~~~
-
-
+// ~~~~~~~~~~~~~~~~~~ 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sldyvva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,12 +25,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // collections
+    const carCollection = client.db('carDB').collection('car');
+    // read
+    app.get('/car', async(req,res) =>{
+      const cursor = carCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // creat
+    app.post('/car', async(req,res)=>{
+      const newCar = req.body;
+      const result = await carCollection.insertOne(newCar);
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
